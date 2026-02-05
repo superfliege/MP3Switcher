0c9afe8652799af9fcd65b32241551878e69fed8
superfliege <superfliege@superfliege.de>
2026-02-06 00:05:16 +0100

    Add initial HTML, manifest, and service worker files for Audio A/B Switcher

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'ab-switcher-v1';
+const urlsToCache = [
+  '/',
+  '/index.html'
+];
+
+// Installation
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+// Aktivierung
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch-Handler
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
